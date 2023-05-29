322a84946c8847b08fd8e5832e6fd826008ac704
Pranav Khatri <khatripranav98@gmail.com>
2023-05-29 14:14:25 +0530

    added post request with shopId payload

--- src/routes/reviews.controller.js
+++ src/routes/reviews.controller.js
@@ -142,15 +142,113 @@ async function httpAddNewReview(req, res){
 
    
     // reviews.push(new_review);
-    
+
     await addNewReview(review);
 
     return res.status(201).json(review);
 
 }
 
+async function httpPostReviewByShopId(req, res){
+
+    const shopId = Number(req.body.shopId);
+
+    // const filteredReview = reviews.filter((review) => review.shopId == shopId);
+    const filteredReview = await existsReviewByShopId(shopId);
+
+    console.log('filteredReview', filteredReview)
+
+    if(filteredReview){
+        const resReviews = [];
+
+        const groupedReviews = {};
+        filteredReview.forEach((each_review) => {
+            console.log('each_review', each_review)
+            each_review = each_review.toObject();
+        const { customerId, customerName,review, ...reviewWithoutCustomerInfo } = each_review;
+        if (!groupedReviews.hasOwnProperty(each_review.productId)) {
+            groupedReviews[each_review.productId] = [];
+        }
+        groupedReviews[each_review.productId].push(reviewWithoutCustomerInfo);
+        });
+
+        console.log('groupedReviews', groupedReviews)
+
+        for(const productId in groupedReviews){
+            if(groupedReviews.hasOwnProperty(productId)){
+                const reviews = groupedReviews[productId];
+
+                console.log('reviews',reviews )
+
+                let totalRating = 0;
+                const ratingCounts = {
+                    totalRatingCount:  0,
+                    oneStarRatingCount: 0,
+                    twoStarRatingCount: 0,
+                    threeStarRatingCount: 0,
+                    fourStarRatingCount: 0,
+                    fiveStarRatingCount: 0,
+                };
+
+                reviews.forEach((review)=>{
+                    const rating = review.rating;
+
+                    ratingCounts.totalRatingCount++;
+
+                    switch (rating) {
+                        case 1:
+                          ratingCounts.oneStarRatingCount++;
+                          break;
+                        case 2:
+                            ratingCounts.twoStarRatingCount++;
+                          break;
+                        case 3:
+                          ratingCounts.threeStarRatingCount++;
+                          break;
+                        case 4:
+                          ratingCounts.fourStarRatingCount++;
+                          break;
+                        case 5:
+                          ratingCounts.fiveStarRatingCount++;
+                          break;
+                        default:
+                          break;
+                      }
+                    
+
+                      totalRating += rating;
+
+                });
+
+                const averageRating =  ratingCounts.totalRatingCount > 0 ? totalRating /  ratingCounts.totalRatingCount : 0;
+
+                const new_review = {
+                    shopId:shopId,
+                    productId:productId,
+                    ...ratingCounts,
+                    averageRating:averageRating
+
+                }
+
+                resReviews.push(new_review);
+
+            }
+        }
+
+
+        res.status(200).json(resReviews);
+    }
+    else{
+        res.status(404).json({
+            error: "Oops Shop Id donot Exist!"
+        });
+    }
+
+}
+
 module.exports = {
     httpGetAllReviews,
     httpGetReviewByShopId,
-    httpAddNewReview
+    httpAddNewReview,
+    httpPostReviewByShopId
 };
